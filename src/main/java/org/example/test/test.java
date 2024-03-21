a08e42c160ae6ad87c29e3f24b25448bb8722eea
kimdongmin <meena2003@naver.com>
2024-03-21 15:14:09 +0900

    C

--- src/main/java/org/example/test/test.java
+++ src/main/java/org/example/test/test.java
@@ -5,5 +5,6 @@ public class test {
     public test() {
         System.out.println("true = " + true);
         System.out.println("true = " + true);
+        System.out.println("true = " + true);
     }
 }
